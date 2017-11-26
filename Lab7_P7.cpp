55129a8ced6a857200c39b16550bd146e7a5d30e
Vlad <vladeas@users.noreply.github.com>
2017-11-26 19:58:28 +0200

    Update Lab7_P7.cpp

--- Lab7_P7.cpp
+++ Lab7_P7.cpp
@@ -12,9 +12,11 @@ int main()
 	int nr;
 	cout << "<<< Insert number : ";
 	cin >> nr;
+	
 	for (int i = 1; i <= nr / 2; i++)
 		if (nr%i == 0)
 			cout << i << "   ";
+	
 	cout << nr << endl;
 	system("pause");
 }
