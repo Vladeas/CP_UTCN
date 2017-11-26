d0b2cfe3ff6b2b25525f88311acdadc46a22ac86
Vlad <vladeas@users.noreply.github.com>
2017-11-26 18:16:05 +0200

    Update Lab7_P1.cpp

--- Lab7_P1.cpp
+++ Lab7_P1.cpp
@@ -14,21 +14,29 @@ int check(int a, int b, int c);
 int main()
 {
 	int a, b, c;
+	
 	cout << "<<< Insert the two lenghts of the rectangle, and its diagonal :\n";
 	cin >> a >> b >> c;
+	
 	cout << ">>> The area of the rectangle is " << area(a,b) << endl;
+	
 	if (check(a, b, c))
 		cout << ">>> The given lenght is a diagonal\n";
 	else 
 		cout << ">>> The given lenght is not a diagonal\n";
+	
 	system("pause");
 }
 
+
+//The area of the rectangle
 int area(int a, int b)
 {
 	return a*b;
 }
 
+
+//Check with Pythagoras formula if C can be diagonal
 int check(int a, int b, int c)
 {
 	if (a*a + b*b == c*c)
