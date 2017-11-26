24a3f7e9c9352c5290a2ed3faad94a8726b2b8bb
Vlad <vladeas@users.noreply.github.com>
2017-11-26 19:14:35 +0200

    Update Lab7_P2.cpp

--- Lab7_P2.cpp
+++ Lab7_P2.cpp
@@ -14,7 +14,7 @@ int main()
 	int a;
 	cout << "<<< Insert a number :\n";
 	cin >> a;
-	if (sqrt(a) == int(sqrt(a)))
+	if (sqrt(a) == int(sqrt(a))) // Check if the float value of sqrt is equal to its integer value
 		cout << ">>> The number is a perfect square.\n";
 	else
 		cout << ">>> The number is not a perfect square.\n";
