96ee94b5e5521d8ad5d6e9d88dca192ba3acdf72
Vlad <vladeas@users.noreply.github.com>
2017-11-26 18:52:03 +0200

    Update Lab7_P13.cpp

--- Lab7_P13.cpp
+++ Lab7_P13.cpp
@@ -8,21 +8,35 @@
 #include <iostream>
 using namespace std;
 
+int cmmmc(int a,int b);
+
 int main() {
-	int a, b, prod;
+	int a, b;
 	cout << "<<< First number : ";
 	cin >> a;
 	cout << "<<< Second number : ";
 	cin >> b;
+	cout << ">>> The least common multiple is : " << cmmmc(a,b) << endl;
+	system("pause");
+	return 0;
+}
+
+
+/*
+>>> Solve the problem by using the formul a*b = cmmdc(a,b)*cmmmc(a,b)
+#Input : a,b(Two integer numbers)
+#Output : cmmmc(Of a and b)
+*/
+int cmmmc(int a,int b)
+{
+	int prod;
 	prod = a*b;
-	while (a != b) 
+	while (a != b)	//calculate cmmdc of a and b
 	{
 		if (a>b)
 			a = a - b;
 		else
 			b = b - a;
 	}
-	cout << ">>> The least common multiple is : " << prod / a << endl;
-	system("pause");
-	return 0;
+	return prod / a;
 }
